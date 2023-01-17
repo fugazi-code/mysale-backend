8fdae83f638e8d469e652f2e0fddf0a6aee63f19
Renier Trenuela <renier.trenuela@gmail.com>
2023-01-17 08:18:56 +0800

    update

--- webview/src/components/NavbarComponent.vue
+++ webview/src/components/NavbarComponent.vue
@@ -1,28 +1,38 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-0">
         <div class="collapse navbar-collapse h-100" id="navbarNav">
-            <div class="container h-100">
+            <div class="container h-100 d-flex justify-content-between">
                 <ul class="navbar-nav h-100">
                     <router-link class="navbar-brand my-auto" :to="{ name: 'home' }">
                         <img src="../assets/nav-logo.svg" alt="mysale logo" width="120" height="34" />
                     </router-link>
-                    <li class="nav-item pt-2">
+                    <li class="nav-item pt-3 me-2">
                         <div class="input-group mb-3">
                             <label class="input-group-text" for="inputGroupSelect01">
                                 <i class="fa-solid fa-location-pin"></i>
                             </label>
                             <select class="form-select" id="inputGroupSelect01" v-model="selectedCity">
-                                <option value="">Choose...</option>
+                                <option value="">All Cities</option>
                                 <option v-for="city in cities" :value="city.id">{{ city.name_en }}</option>
                             </select>
                         </div>
-
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link h-100 pt-3 px-4" :to="{ name: 'home' }">Car and Bikes</router-link>
+                        <router-link class="nav-link h-100 pt-3 px-2" :to="{ name: 'home' }">Car and Bikes</router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link class="nav-link h-100 pt-3 px-4" :to="{ name: 'about' }">About</router-link>
+                        <router-link class="nav-link h-100 pt-3 px-2" :to="{ name: 'about' }">About</router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav h-100">
+                    <li class="nav-item">
+                        <router-link class="nav-link h-100 pt-3 px-2" :to="{ name: 'home' }">Log In</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link h-100 pt-3 px-2" :to="{ name: 'home' }">Sign Up</router-link>
+                    </li>
+                    <li class="nav-item h-100 ms-2">
+                        <button type="button" class="btn btn-info mt-2 text-white">Sell</button>
                     </li>
                 </ul>
             </div>
@@ -48,7 +58,7 @@ export default {
             cities: []
         };
     },
-    mounted(){
+    mounted() {
         this.isActive();
     }
 }
